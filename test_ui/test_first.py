1587b015a0224e6473a69cedec4dafb43d23dc78
Kastanov70 <stepan.kastanov@gmail.com>
2021-11-30 17:10:51 +0300

    yml config

--- test_ui/test_first.py
+++ test_ui/test_first.py
@@ -4,15 +4,14 @@ from playwright.async_api import Playwright
 
 @pytest.mark.smoke
 def test_for_start(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=True, slow_mo=500)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://www.google.com/")
-    page.click("[aria-label=\"Найти\"]")
-    page.fill("[aria-label=\"Найти\"]", "playwright")
-    page.press("[aria-label=\"Найти\"]", "Tab")
-    with page.expect_navigation():
-        page.click(":nth-match(:text(\"Поиск в Google\"), 2)")
-
-    page.click("text=https://playwright.dev")
-    assert page.url == "https://playwright.dev/"
+    page.goto("https://skillotron.com/")
+    # Go to https://skillotron.com/signin
+    page.goto("https://skillotron.com/signin")
+    # Click text=News
+    page.click("text=News")
+    assert page.url == "https://skillotron.com/news"
+    # Click h1:has-text("News")
+    assert page.text_content("h1:has-text(\"News\")") == "News"
