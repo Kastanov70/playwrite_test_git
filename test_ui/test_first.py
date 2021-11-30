610870e0a4003fe5caf96ebc0f16db6a1018ae9e
Kastanov70 <stepan.kastanov@gmail.com>
2021-11-30 15:21:36 +0300

    first commit

---
+++ test_ui/test_first.py
@@ -0,0 +1,16 @@
+import pytest
+from playwright.async_api import Playwright
+
+
+@pytest.mark.smoke
+def test_for_start(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.google.com/")
+    page.click("[aria-label=\"Найти\"]")
+    page.fill("[aria-label=\"Найти\"]", "playwright")
+    with page.expect_navigation():
+        page.press("[aria-label=\"Найти\"]", "Enter")
+    page.click("text=https://playwright.dev")
+    assert page.url == "https://playwright.dev/"
